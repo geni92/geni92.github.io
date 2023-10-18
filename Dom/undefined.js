bab9dee7c24827af6cfdc340852bf0a9f1077246
geni92 <147835284+geni92@users.noreply.github.com>
2023-10-18 10:49:08 -0500

    Delete Dom directory

--- Dom/undefined.js
+++
@@ -1,7 +0,0 @@
-function greaterthan5(num){
-     if (num >5){
-        return true;
-     }
-}
-console.log(greaterthan5(4))// since we didnt have return false statement it returns undefined
-console.log(greaterthan5(10))
